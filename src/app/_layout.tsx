7fec7e1b382520368bd9fa61ad532f73f08ff0b0
Sonny Sangha <sonny.sangha@gmail.com>
2025-06-24 16:24:05 +0400

    Add @expo/vector-icons dependency and implement tab navigation in layout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,40 @@
 import "../global.css";
-import { Slot } from "expo-router";
+import { Slot, Stack, Tabs } from "expo-router";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
 export default function Layout() {
-  return <Slot />;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="user" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="history"
+        options={{
+          title: "History",
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <AntDesign name="clockcircle" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
---
+++ src/app/history.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+export default function Page() {
+  return (
+    <SafeAreaView className="flex flex-1">
+      <Text>History</Text>
+    </SafeAreaView>
+  );
+}
